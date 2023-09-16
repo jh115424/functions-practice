6555203568e58ba1607c03d506b6149687db2a3a
jennifer hodge <jenniferhodge@jennifers-Air.lan>
2023-09-15 19:03:00 -0500

    moving along on ex 2....

--- exercises/e2.js
+++ exercises/e2.js
@@ -4,25 +4,20 @@
 /**
  * ====================================================
  * Create a function that works as follows:
-   * The function name is 'rectArea'
-   * The function returns the value of the area of a rectangle (sideA * sideB)
-   * The function takes 2 parameters (two sides) to calculate the area from
+ * The function name is 'rectArea'
+ * The function returns the value of the area of a rectangle (sideA * sideB)
+ * The function takes 2 parameters (two sides) to calculate the area from
  * rectArea(3, 2) => 6
-*/
+ */
 
 // Your function code starts here...
-
-
 
 let area = rectArea(3, 2);
 
 function rectArea(sideA, sideB) {
-
   return sideA * sideB;
 
   console.log(area);
-
-
 }
 
 // Use the function you created above to calculate the areas of 3 rooms
@@ -31,27 +26,19 @@ function rectArea(sideA, sideB) {
 
 // Your code goes here:
 
-
-
-
-function calculate (room1, room2, room3) {
-
+function calculate(room1, room2, room3) {
   return room1 * room2 * room3;
 
+  console.log(room1);
+  console.log(room2);
+  console.log(room3);
 
-
-console.log(room1);
-console.log(room2);
-console.log(room3);
-
-
+  console.log(4 * 6 + 13 * 3 + 4 * 8);
 }
 
 let room1 = 4 * 6;
 let room2 = 13 * 3;
 let room3 = 4 * 8;
-
-
 
 /**
  * =============================================================
@@ -64,28 +51,33 @@ let room3 = 4 * 8;
  */
 
 // Your code goes here...
+ 
+function totalRoomsArea() {
+  return totalRoomsArea(area1, area2, area3);
 
+  let area1 = 24;
+  let area2 = 35;
+  let area3 = 24;
 
+  console.log(totalRoomsArea);
+}
 
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-    console.log(/* replace this comment with a function call here... */);
-}
-
-
+  console.log(/* replace this comment with a function call here... */);
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
-    rectArea: rectArea || undefined,
-    roomVars: [room1 || undefined, room2 || undefined, room3 || undefined],
-    totalRoomsArea: totalRoomsArea || undefined,
-    result: (room1 && room2 && room3) ? result : undefined,
-}
+  rectArea: rectArea || undefined,
+  roomVars: [room1 || undefined, room2 || undefined, room3 || undefined],
+  totalRoomsArea: totalRoomsArea || undefined,
+  result: room1 && room2 && room3 ? result : undefined,
+};
 export { f };
