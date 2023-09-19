91bc35624c6bc2d8a01700b2b253e2e7a2c91914
jennifer hodge <jenniferhodge@jennifers-Air.lan>
2023-09-19 18:06:03 -0500

    got some help from Zak and Shane on the syntax

---
+++ testing.js
@@ -0,0 +1,8 @@
+
+function getValueWithConditionOne(a, b) {
+    if (a === 40 && b === 40) {
+      return a + b;
+    } else return (a + b) * 2;
+  }
+  
+  console.log(getValueWithConditionOne(20, 30));
